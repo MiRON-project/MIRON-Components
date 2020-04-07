dfb9c4965425ed3043dae6cf34fe8110e9821ddf
renan028 <renan028@gmail.com>
2020-04-06 23:35:00 -0300

    fix setting pose in lidar scan

--- ComponentWebotsRobot/smartsoft/src/LidarTask.cc
+++ ComponentWebotsRobot/smartsoft/src/LidarTask.cc
@@ -98,6 +98,7 @@ int LidarTask::on_execute()
 			scan.set_scan_index(i, i);
 			scan.set_scan_integer_distance(i, dist);
 		}
+		scan.set_base_state(*COMP->_pose.get());
 		scan.set_scan_valid(true);
 	}
 	else
