021756517c111d7bf3ac42e5e429b980f5182bcc
renan028 <renan028@gmail.com>
2020-08-10 06:28:57 -0300

    add Goto skill for global planner

--- ComponentWebotsPlanner/smartsoft/src/StaticGlobalPlanner.cc
+++ ComponentWebotsPlanner/smartsoft/src/StaticGlobalPlanner.cc
@@ -19,18 +19,18 @@
 
 #include <iostream>
 
-StaticGlobalPlanner::StaticGlobalPlanner(SmartACE::SmartComponent *comp) : 
-	StaticGlobalPlannerCore(comp),
-	replan(false)
+StaticGlobalPlanner::StaticGlobalPlanner(SmartACE::SmartComponent *comp) :
+    StaticGlobalPlannerCore(comp),
+    replan(false)
 {
-	auto box = COMP->getGlobalState().getRobot().getFootprint();
-	std::vector<double> v_box;
-	for (auto &b : box)
-		v_box.push_back(b);
-	robot_footprint = Eigen::AlignedBox2d(
-			Eigen::Vector2d(v_box[0], v_box[1]), Eigen::Vector2d(v_box[2], v_box[3]));
-	space = std::make_shared<ompl::base::SE2StateSpace>();
-	simple_setup_ = std::make_shared<ompl::geometric::SimpleSetup>(space);
+  auto box = COMP->getGlobalState().getRobot().getFootprint();
+  std::vector<double> v_box;
+  for (auto &b : box)
+      v_box.push_back(b);
+  robot_footprint = Eigen::AlignedBox2d(
+      Eigen::Vector2d(v_box[0], v_box[1]), Eigen::Vector2d(v_box[2], v_box[3]));
+  space = std::make_shared<ompl::base::SE2StateSpace>();
+  simple_setup_ = std::make_shared<ompl::geometric::SimpleSetup>(space);
 }
 
 StaticGlobalPlanner::~StaticGlobalPlanner()
@@ -39,102 +39,116 @@ StaticGlobalPlanner::~StaticGlobalPlanner()
 
 void StaticGlobalPlanner::BaseStateServiceIn(const CommBasicObjects::CommBaseState &input)
 {
-	robot_pose = input;
+    robot_pose = input;
 }
 
 void StaticGlobalPlanner::ObstaclesServiceIn(
-		const CommNavigationObjects::BoundingBoxes &input)
+    const CommNavigationObjects::BoundingBoxes &input)
 {
-	sample_space = input.getFloor();
+  sample_space = input.getFloor();
 
-	ompl::base::RealVectorBounds bounds(2);
-	bounds.setLow(0, std::min(sample_space.getXmin(), sample_space.getXmax()));
-	bounds.setLow(1, std::min(sample_space.getZmin(), sample_space.getZmax()));
-	bounds.setHigh(0, std::max(sample_space.getXmax(), sample_space.getXmin()));
-	bounds.setHigh(1, std::max(sample_space.getZmin(), sample_space.getZmax()));
-	space->setBounds(bounds);
+  ompl::base::RealVectorBounds bounds(2);
+  bounds.setLow(0, std::min(sample_space.getXmin(), sample_space.getXmax()));
+  bounds.setLow(1, std::min(sample_space.getZmin(), sample_space.getZmax()));
+  bounds.setHigh(0, std::max(sample_space.getXmax(), sample_space.getXmin()));
+  bounds.setHigh(1, std::max(sample_space.getZmin(), sample_space.getZmax()));
+  space->setBounds(bounds);
 
-	auto boxes = input.getBoxesCopy();
-	for (auto &box : boxes)
-	{
-		Eigen::Vector2d _min(
-				std::min(box.getXmin(), box.getXmax()),
-				std::min(box.getZmin(), box.getZmax()));
-		Eigen::Vector2d _max(
-				std::max(box.getXmin(), box.getXmax()),
-				std::max(box.getZmin(), box.getZmax()));
-		obstacles.push_back(Eigen::AlignedBox2d(_min, _max));
-	}
+  auto boxes = input.getBoxesCopy();
+  for (auto &box : boxes)
+  {
+    Eigen::Vector2d _min(
+        std::min(box.getXmin(), box.getXmax()),
+        std::min(box.getZmin(), box.getZmax()));
+    Eigen::Vector2d _max(
+        std::max(box.getXmin(), box.getXmax()),
+        std::max(box.getZmin(), box.getZmax()));
+    obstacles.push_back(Eigen::AlignedBox2d(_min, _max));
+  }
 
-	validity_checker = std::make_shared<BoundingBoxValidityChecker>(
-			simple_setup_->getSpaceInformation(), obstacles, robot_footprint);
-	simple_setup_->setStateValidityChecker(validity_checker);
-	obstacles_init = true;
+  validity_checker = std::make_shared<BoundingBoxValidityChecker>(
+      simple_setup_->getSpaceInformation(), obstacles, robot_footprint);
+  simple_setup_->setStateValidityChecker(validity_checker);
+  obstacles_init = true;
 }
 
-void StaticGlobalPlanner::PlannerGoalServiceIn(
-		const CommNavigationObjects::CommPlannerGoal &input)
+CommRobotinoObjects::CommPathNavigationGoal StaticGlobalPlanner::PlannerGoalServiceIn(
+    const CommNavigationObjects::CommPlannerGoal &input)
 {
-	if (input.getXGoalPoint() == goal.getXGoalPoint() &&
-			input.getYGoalPoint() == goal.getYGoalPoint() && !replan)
-		return;
-	if (validity_checker)
-	{
-		goal = input;
-		ompl::base::ScopedState<ompl::base::SE2StateSpace> start(space);
-		ompl::base::ScopedState<ompl::base::SE2StateSpace> end(space);
-		start->setXY(robot_pose.get_base_position().get_x(1),
-			-robot_pose.get_base_position().get_y(1));
-		end->setXY(input.getXGoalPoint(), input.getYGoalPoint());
-		simple_setup_->setStartAndGoalStates(start, end);
-		ompl::base::PlannerStatus solved = simple_setup_->solve(1.0);
-		if (solved)
-		{
-			simple_setup_->simplifySolution();
-			simple_setup_->getSolutionPath().print(std::cout);
-		}
-	}
+  CommRobotinoObjects::CommPathNavigationGoal path_navigation_goal;
+  if (input.getXGoalPoint() == goal.getXGoalPoint() &&
+      input.getYGoalPoint() == goal.getYGoalPoint() && !replan)
+      return path_navigation_goal;
+  if (validity_checker)
+  {
+    std::cout << "got planner query: " << input.getXGoalPoint() << ", " << input.getYGoalPoint() << "\n";
+    goal = input;
+    ompl::base::ScopedState<ompl::base::SE2StateSpace> start(space);
+    ompl::base::ScopedState<ompl::base::SE2StateSpace> end(space);
+    start->setXY(robot_pose.get_base_position().get_x(1),
+        -robot_pose.get_base_position().get_y(1));
+    end->setXY(input.getXGoalPoint(), input.getYGoalPoint());
+    simple_setup_->setStartAndGoalStates(start, end);
+    ompl::base::PlannerStatus solved = simple_setup_->solve(1.0);
+    if (solved)
+    {
+      simple_setup_->simplifySolution();
+      simple_setup_->getSolutionPath().print(std::cout);
+      auto states = simple_setup_->getSolutionPath().getStates();
+
+      std::vector<CommRobotinoObjects::CommPathNode> nodes;
+      for (auto& state : states) {
+          CommRobotinoObjects::CommPathNode node;
+          const auto xy = state->as<ompl::base::SE2StateSpace::StateType>();
+          node.setX(xy->getX());
+          node.setY(xy->getY());
+          nodes.push_back(node);
+      }
+      path.setNodes(nodes);
+      path_navigation_goal.setPaths(path);
+      path_navigation_goal.setGoal(robot_pose.get_base_position().get_x(1),
+          -robot_pose.get_base_position().get_y(1), 0, input.getXGoalPoint(),
+          input.getYGoalPoint(), states.size());
+      pathNavigationGoalServiceOutPut(path_navigation_goal);
+    }
+  }
+  return path_navigation_goal;
 }
 
 int StaticGlobalPlanner::on_entry()
 {
-	return 0;
+  return 0;
 }
 
 int StaticGlobalPlanner::on_execute()
 {
-	COMP->mRobotMutex.acquire();
-	CommNavigationObjects::BoundingBoxes objs;
-	Smart::StatusCode obj_status = obstaclesServiceInGetUpdate(objs);
-	if (obj_status == Smart::SMART_OK)
-	{
-		obstacles_init = false;
-		ObstaclesServiceIn(objs);
-	}
-	if (!obstacles_init)
-	{
-		COMP->mRobotMutex.release();
-		return 0;
-	}
+  COMP->mRobotMutex.acquire();
+  CommNavigationObjects::BoundingBoxes objs;
+  Smart::StatusCode obj_status = obstaclesServiceInGetUpdate(objs);
+  if (obj_status == Smart::SMART_OK)
+  {
+    obstacles_init = false;
+    ObstaclesServiceIn(objs);
+  }
+  if (!obstacles_init)
+  {
+    COMP->mRobotMutex.release();
+    return 0;
+  }
 
-	CommBasicObjects::CommBaseState base;
-	Smart::StatusCode base_status = baseStateServiceInGetUpdate(base);
-	if (base_status != Smart::SMART_OK)
-	{
-		COMP->mRobotMutex.release();
-		return 0;
-	}
-	BaseStateServiceIn(base);
-
-	CommNavigationObjects::CommPlannerGoal goal_;
-	goal_.setXGoalPoint(1.6);
-	goal_.setYGoalPoint(7.76);
-	PlannerGoalServiceIn(goal_);
-	COMP->mRobotMutex.release();
-	return 0;
+  CommBasicObjects::CommBaseState base;
+  Smart::StatusCode base_status = baseStateServiceInGetUpdate(base);
+  if (base_status != Smart::SMART_OK)
+  {
+    COMP->mRobotMutex.release();
+    return 0;
+  }
+  BaseStateServiceIn(base);
+  COMP->mRobotMutex.release();
+  return 0;
 }
 int StaticGlobalPlanner::on_exit()
 {
-	// use this method to clean-up resources which are initialized in on_entry() and needs to be freed before the on_execute() can be called again
-	return 0;
+    // use this method to clean-up resources which are initialized in on_entry() and needs to be freed before the on_execute() can be called again
+    return 0;
 }
--- ComponentWebotsPlanner/smartsoft/src/StaticGlobalPlanner.hh
+++ ComponentWebotsPlanner/smartsoft/src/StaticGlobalPlanner.hh
@@ -27,63 +27,64 @@
 
 class BoundingBoxValidityChecker : public ompl::base::StateValidityChecker
 {
-	std::vector<Eigen::AlignedBox2d> obstacles;
-	Eigen::AlignedBox2d robot;
+  std::vector<Eigen::AlignedBox2d> obstacles;
+  Eigen::AlignedBox2d robot;
 
 public:
-	BoundingBoxValidityChecker(const ompl::base::SpaceInformationPtr &si,
-      const std::vector<Eigen::AlignedBox2d> &_obstacles,
-      const Eigen::AlignedBox2d &_robot) : 
+  BoundingBoxValidityChecker(const ompl::base::SpaceInformationPtr &si,
+    const std::vector<Eigen::AlignedBox2d> &_obstacles,
+    const Eigen::AlignedBox2d &_robot) :
     ompl::base::StateValidityChecker(si),
     obstacles(_obstacles),
     robot(_robot) {}
 
-	virtual bool isValid(const ompl::base::State *state) const
-	{
-		const auto *xy = state->as<ompl::base::SE2StateSpace::StateType>();
-		double x = xy->getX();
-		double y = xy->getY();
-		Eigen::Vector2d point(x, y);
-		Eigen::AlignedBox2d translated_robot(robot);
-		translated_robot.translate(point);
-		for (auto &obstacle : obstacles)
-		{
-			if (obstacle.intersects(translated_robot))
-			{
-				return false;
-			}
-		}
-		return true;
-	}
+  virtual bool isValid(const ompl::base::State *state) const
+  {
+    const auto *xy = state->as<ompl::base::SE2StateSpace::StateType>();
+    double x = xy->getX();
+    double y = xy->getY();
+    Eigen::Vector2d point(x, y);
+    Eigen::AlignedBox2d translated_robot(robot);
+    translated_robot.translate(point);
+    for (auto &obstacle : obstacles)
+    {
+      if (obstacle.intersects(translated_robot))
+      {
+          return false;
+      }
+    }
+    return true;
+  }
 };
 
 class StaticGlobalPlanner : public StaticGlobalPlannerCore
 {
 private:
-	CommBasicObjects::CommBaseState robot_pose;
-	std::vector<Eigen::AlignedBox2d> obstacles;
-	bool obstacles_init;
-	bool replan;
-	CommNavigationObjects::CommPlannerGoal goal;
-	Eigen::AlignedBox2d robot_footprint;
-	CommNavigationObjects::BoundingBox sample_space;
+  CommBasicObjects::CommBaseState robot_pose;
+  std::vector<Eigen::AlignedBox2d> obstacles;
+  bool obstacles_init;
+  bool replan;
+  CommNavigationObjects::CommPlannerGoal goal;
+  Eigen::AlignedBox2d robot_footprint;
+  CommNavigationObjects::BoundingBox sample_space;
+  CommRobotinoObjects::CommNavigationPaths path;
 
-	std::shared_ptr<ompl::base::SE2StateSpace> space;
-	ompl::geometric::SimpleSetupPtr simple_setup_;
-	ompl::base::StateValidityCheckerPtr validity_checker;
-	ompl::base::MotionValidatorPtr motion_checker;
+  std::shared_ptr<ompl::base::SE2StateSpace> space;
+  ompl::geometric::SimpleSetupPtr simple_setup_;
+  ompl::base::StateValidityCheckerPtr validity_checker;
+  ompl::base::MotionValidatorPtr motion_checker;
 
-	void BaseStateServiceIn(const CommBasicObjects::CommBaseState &input);
-	void ObstaclesServiceIn(const CommNavigationObjects::BoundingBoxes &input);
-	void PlannerGoalServiceIn(const CommNavigationObjects::CommPlannerGoal &input);
+  void BaseStateServiceIn(const CommBasicObjects::CommBaseState &input);
+  void ObstaclesServiceIn(const CommNavigationObjects::BoundingBoxes &input);
 
 public:
-	StaticGlobalPlanner(SmartACE::SmartComponent *comp);
-	virtual ~StaticGlobalPlanner();
+  StaticGlobalPlanner(SmartACE::SmartComponent *comp);
+  CommRobotinoObjects::CommPathNavigationGoal PlannerGoalServiceIn(const CommNavigationObjects::CommPlannerGoal &input);
+  virtual ~StaticGlobalPlanner();
 
-	virtual int on_entry();
-	virtual int on_execute();
-	virtual int on_exit();
+  virtual int on_entry();
+  virtual int on_execute();
+  virtual int on_exit();
 };
 
 #endif
