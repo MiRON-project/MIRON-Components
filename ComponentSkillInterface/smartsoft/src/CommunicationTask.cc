67e54597f8aa4174daaf365d764f0d1b47562e8d
renan028 <renan028@gmail.com>
2020-09-29 09:44:18 -0300

    add abort skill

--- ComponentSkillInterface/smartsoft/src/CommunicationTask.cc
+++ ComponentSkillInterface/smartsoft/src/CommunicationTask.cc
@@ -262,7 +262,17 @@ int CommunicationTask::separateQuery(std::deque <std::string> &in, std::deque <s
 			}
 
 			else if(parsed_query.query_type == "abort-current-skill"){
-
+				auto parsed_parameter = ParsedQueryParameterChanged(
+					parsed_query);
+				Smart::StatusCode status;
+				SmartACE::CommParameterResponse response;
+				status = COMP->paramMaster->sendParameterWait(
+					parsed_parameter.buildRequest(), response, 
+					parsed_parameter.component, "param");
+				status = COMP->paramMaster->sendParameterWait(
+					parsed_parameter.commitRequest(), response, 
+					parsed_parameter.component, "param");
+				it = in.erase(it);
 			}
 
 			else if(parsed_query.query_type == "change-parameter"){
